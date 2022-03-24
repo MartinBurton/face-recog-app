8d26a2609706647d13280685304315c705096486
MartinBurton <martin_burton@hotmail.com>
2022-03-24 11:43:52 +0000

    Use deployed server

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch(' https://nameless-woodland-66106.herokuapp.com/signin', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
